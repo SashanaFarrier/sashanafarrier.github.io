f15ffb8938c3bebb2e2c0f6f964d1d5e51939684
Sashana Farrier <fionafarrier23@gmail.com>
2024-03-09 10:12:32 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+gsap.registerPlugin(gsap);
+
+// const tl = gsap.timeline({
+//     scrollTrigger: {
+//       trigger: ".hero",
+//       pin: true, // pin the trigger element while active
+//       start: "top top", // when the top of the trigger hits the top of the viewport
+//       end: "+=500", // end after scrolling 500px beyond the start
+//       scrub: 2, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
+//     },
+//   });
