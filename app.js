a4618f8936aea005eaefc9adc896243bf65fc6fe
Sashana Farrier <fionafarrier23@gmail.com>
2024-04-02 10:59:38 -0500

    update files

--- app.js
+++ app.js
@@ -1,11 +1,40 @@
 gsap.registerPlugin(gsap);
 
-// const tl = gsap.timeline({
-//     scrollTrigger: {
-//       trigger: ".hero",
-//       pin: true, // pin the trigger element while active
-//       start: "top top", // when the top of the trigger hits the top of the viewport
-//       end: "+=500", // end after scrolling 500px beyond the start
-//       scrub: 2, // smooth scrubbing, takes 1 second to "catch up" to the scrollbar
-//     },
-//   });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const headerBar = document.querySelector(".header-bar");
+    const header = document.querySelector("header");
+    var initialWidth = header.clientWidth;
+  
+    window.addEventListener('scroll', function() {
+      var scrollPercentage = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
+     
+        let newWidth;
+      if(scrollPercentage > 0) {
+        newWidth = initialWidth * (1 - scrollPercentage);
+      } else {
+        newWidth = initialWidth * (1 + scrollPercentage);
+      }
+
+      headerBar.style.width = newWidth + 'px';
+
+      if(this.window.scrollY > 1500) {
+        header.style.position = "sticky";
+        header.style.top = "0";
+        header.style.opacity = "0.8"
+      } else {
+        header.style.position = "";
+        header.style.top = "";
+        header.style.opacity = ""
+      }
+    });
+  });
+
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     const intersecting = entry.isIntersecting
+//     entry.target.style.backgroundColor = intersecting ? "blue" : "orange"
+//   })
+// })
+
+// observer.observe(document.querySelector(".grid-section"))
