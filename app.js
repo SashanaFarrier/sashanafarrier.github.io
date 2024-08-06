8a35ded98d7578e988b4ada1bbbeaaeb0b9dd112
sashanafarrier <fionafarrier23@gmail.com>
2024-08-06 15:54:10 -0500

    Minor changes made.Added form submit functionality

--- app.js
+++ app.js
@@ -38,3 +38,9 @@ document.addEventListener('DOMContentLoaded', function() {
 // })
 
 // observer.observe(document.querySelector(".grid-section"))
+
+//reset form after submission
+    window.onload = function() {
+        // Reset the form fields when the page loads
+        document.getElementById("contact-form").reset();
+    };
