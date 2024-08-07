ea815beabe29d055684bbed48d97bd3f40ec0d13
Sashana Farrier <65877581+SashanaFarrier@users.noreply.github.com>
2024-08-06 20:58:26 -0500

    Add animations to dropdown menu

--- app.js
+++ app.js
@@ -44,3 +44,34 @@ document.addEventListener('DOMContentLoaded', function() {
         // Reset the form fields when the page loads
         document.getElementById("contact-form").reset();
     };
+
+    const tl = gsap.timeline();
+
+    const hamburgerMenus = Array.from(document.querySelectorAll(".hamburger"));
+    hamburgerMenus.forEach(icon => {
+      icon.addEventListener("click", (e)=> {
+      const UL = e.currentTarget.parentElement.querySelector("ul");
+      if(UL.style.display == "block") {
+        UL.style.display = "none"
+      } else {
+          UL.style.display = "block";
+          const LIs = Array.from(UL.querySelectorAll("li"))
+          
+          LIs.forEach((li, index) => {
+            tl.fromTo(li, {
+              opacity: 0,
+              y: 0
+            }, {
+              ease: "bounce.out",
+              opacity: 1,
+              y: 10,
+              duration: 0.2, 
+              delay: index * 0.1 
+            });
+        });
+      }
+     
+   
+    });
+
+    });
